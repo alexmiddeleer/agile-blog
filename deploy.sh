7d7b53fae3649966a09692842ad34684ac2ff8b2
alexMA <almiddeleer@gmail.com>
2019-04-14 11:41:24 -0400

    Restructure app so relative paths aren't hard

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,4 @@ if [[ `git status --porcelain` ]]; then
   echo 'Error: git status not clean. Clean up before deploying.';
   exit 1;
 fi
-rm index.html
-cp ./src/index.html index.html
-git add index.html
-git commit -m "deploy commit"
 git push
--- test/page-test.js
+++ test/page-test.js
@@ -1,12 +1,11 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
 
 fixture `Getting Started`// declare the fixture
-  .page `http://localhost:3000`
-
+  .page `http://localhost:3000`;
 
 //then create a test and place your code there
 test('My first test', async t => {
-    await t
-        // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector('h1').innerText).eql('Agile Blog');
+  await t
+  // Use the assertion to check if the actual header text is equal to the expected one
+    .expect(Selector('h1').innerText).eql('Agile Blog');
 });
