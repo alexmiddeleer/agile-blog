3f458ddb13281413f1fa2fccc765d134bc7048a5
alexMA <almiddeleer@gmail.com>
2019-04-14 12:17:42 -0400

    Create first post

--- test/page-test.js
+++ test/page-test.js
@@ -1,11 +1,22 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
 
-fixture `Getting Started`// declare the fixture
+fixture `Home page`// declare the fixture
   .page `http://localhost:3000`;
 
 //then create a test and place your code there
-test('My first test', async t => {
+test('Title exists', async t => {
   await t
   // Use the assertion to check if the actual header text is equal to the expected one
     .expect(Selector('h1').innerText).eql('Agile Blog');
+});
+
+fixture `First post`// declare the fixture
+  .page `http://localhost:3000/posts/1.html`;
+
+//then create a test and place your code there
+test('First post exists', async t => {
+  await t
+  // Use the assertion to check if the actual header text is equal to the expected one
+    .expect(Selector('h1').innerText).eql('Agile Blog', 'title exists')
+    .expect(Selector('article').innerText).ok('article exists');
 });
