a562088e9f10c7b94a38faaa9f0110692e812065
alexMA <almiddeleer@gmail.com>
2019-04-14 13:20:22 -0400

    Create second post

--- test/page-test.js
+++ test/page-test.js
@@ -20,3 +20,14 @@ test('First post exists', async t => {
     .expect(Selector('h1').innerText).eql('Agile Blog', 'title exists')
     .expect(Selector('article').innerText).ok('article exists');
 });
+
+fixture `Second post`// declare the fixture
+  .page `http://localhost:3000/posts/2.html`;
+
+//then create a test and place your code there
+test('second post exists', async t => {
+  await t
+  // Use the assertion to check if the actual header text is equal to the expected one
+    .expect(Selector('h1').innerText).eql('Agile Blog', 'title exists')
+    .expect(Selector('article').innerText).ok('article exists');
+});
