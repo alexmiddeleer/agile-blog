e28bc033df062b52bf1299d93a798683ba580ec2
alexMA <almiddeleer@gmail.com>
2019-04-14 16:06:28 -0400

    Add links to other posts

--- test/page-test.js
+++ test/page-test.js
@@ -1,28 +1,29 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
+import { ClientFunction } from 'testcafe';
+
+//Returns the URL of the current web page
+const getPageUrl = ClientFunction(() => window.location.href);
 
 fixture `Home page`// declare the fixture
   .page `http://localhost:3000`;
 
-//then create a test and place your code there
 test('Title exists', async t => {
   await t
-  // Use the assertion to check if the actual header text is equal to the expected one
     .expect(Selector('h1').innerText).eql('Agile Blog');
 });
 
-test('Links to posts exit', async t => {
+test('Links to posts work', async t => {
   await t
-  // Use the assertion to check if the actual header text is equal to the expected one
-    .expect(Selector('nav').innerText).ok('nav exists with content');
+    // .expect(Selector('nav').innerText).ok('nav exists with content');
+    .click('nav a')
+    .expect(getPageUrl()).contains('1.html');
 });
 
 fixture `First post`// declare the fixture
   .page `http://localhost:3000/posts/1.html`;
 
-//then create a test and place your code there
 test('First post exists', async t => {
   await t
-  // Use the assertion to check if the actual header text is equal to the expected one
     .expect(Selector('h1').innerText).eql('Agile Blog', 'title exists')
     .expect(Selector('article').innerText).ok('article exists');
 });
@@ -30,10 +31,8 @@ test('First post exists', async t => {
 fixture `Second post`// declare the fixture
   .page `http://localhost:3000/posts/2.html`;
 
-//then create a test and place your code there
 test('second post exists', async t => {
   await t
-  // Use the assertion to check if the actual header text is equal to the expected one
     .expect(Selector('h1').innerText).eql('Agile Blog', 'title exists')
     .expect(Selector('article').innerText).ok('article exists');
 });
