13bec1dcf19c501e5abfac499d23bf4b580f3908
alexMA <almiddeleer@gmail.com>
2019-04-17 21:21:32 -0400

    start work on do all the things

--- src/article-collector.js
+++ src/article-collector.js
@@ -1,14 +1,42 @@
-const { readdirSync, readFileSync } = require('fs');
+const { readdirSync, readFileSync, writeFileSync } = require('fs');
+const articlePageMaker = require('./article-page-maker.js');
+const convert = require('./article-converter.js');
 
 const collectArticles = function(dir) {
   return readdirSync(dir);
 }
 
-const loadArticleFilePath = function(articleFilePath) {
+const loadArticle = function(articleFilePath) {
   return readFileSync(articleFilePath);
+}
+
+const loadTemplate = loadArticle;
+
+const storeArticleHtmlFile = function(filePath, htmlString) {
+  return writeFileSync(filePath, htmlString);
+}
+
+const processArticle = function(articleString, articleTemplateString) {
+  const templateInput = { article: convert(articleString) };
+  return articlePageMaker(articleTemplateString, templateInput);
+}
+
+const ARTICLES_DIR = 'articleDrafts';
+const TEMPLATE = 'templates/article-template.html';
+const templateAllArticles = function() {
+  const articles = collectArticles(ARTICLES_DIR);
+  const template = loadTemplate(TEMPLATE);
+  // const articlesHtml = [];
+  for( let index in articles ){
+    const a = loadArticle(articles[index]);
+  }
 }
 
 module.exports = {
   collectArticles,
-  loadArticleFilePath
+  loadArticle,
+  storeArticleHtmlFile,
+  processArticle,
+  loadTemplate,
+  templateAllArticles
 };
--- src/article-collector.test.js
+++ src/article-collector.test.js
@@ -1,6 +1,10 @@
 jest.mock('fs');
-const { collectArticles, loadArticleFilePath } = require('./article-collector');
+const { processArticle, collectArticles, storeArticleHtmlFile,
+  loadTemplate, loadArticle, templateAllArticles }
+  = require('./article-collector');
 const fs = require('fs');
+
+beforeEach(() => jest.clearAllMocks());
 
 test('collectArticles finds all of the files in the specified directory', function() {
   fs.readdirSync.mockReturnValue(['foo.md']);
@@ -8,8 +12,39 @@ test('collectArticles finds all of the files in the specified directory', functi
   expect(result).toEqual(['foo.md']);
 });
 
-test('load file loads the file', function() {
+test('loadArticle loads the file', function() {
   fs.readFileSync.mockReturnValue('hello world');
-  const result = loadArticleFilePath('src');
+  const result = loadArticle('src');
   expect(result).toEqual('hello world');
+});
+
+test('loadTemplate loads the specified template given a path', function() {
+  fs.readFileSync.mockReturnValue('template');
+  const result = loadTemplate('src');
+  expect(result).toEqual('template');
+});
+
+test('storeArticleHtmlFile write the string to the specified location', function() {
+  storeArticleHtmlFile('article.html', 'my article');
+  expect(fs.writeFileSync.mock.calls[0][0]).toEqual('article.html');
+  expect(fs.writeFileSync.mock.calls[0][1]).toEqual('my article');
+});
+
+test('processArticle converts the article to html and makes a page', function() {
+  const result = processArticle('foo', 'a{{{article}}}a');
+  const expected = 'a<p>foo</p>a';
+  expect(result).toEqual(expected);
+});
+
+test(`templateAllArticles loads all articles and the article template, processes the
+      and stores the templated html files in the
+      desired folder` , function() {
+  fs.readdirSync.mockReturnValue(['foo.md']);
+  fs.readFileSync.mockReturnValueOnce('foo');
+  // fs.readFileSync.mockReturnValueOnce('a{{{article}}}a');
+  templateAllArticles();
+  expect(fs.readdirSync.mock.calls[0][0]).toEqual('articleDrafts');
+  expect(fs.readFileSync.mock.calls[0][0]).toEqual('templates/article-template.html');
+  expect(fs.readFileSync.mock.calls[1][0]).toEqual('foo.md');
+  // expect(fs.readFileSync.mock.calls[0][0]).toEqual('a{{{article}}}a');
 });
